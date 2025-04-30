fb5c627c726c1fe7b2037cb5a658e5e3eac1f6f1
kottak msi <kottakcode@gmail.com>
2025-04-30 19:33:42 +0400

    add js on cat list

---
+++ api_cats.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+
+    const catList = document.getElementById('cats');
+
+    function loadCatList() {
+
+        fetch('http://127.0.0.1:8000/cats/api_cat_list/')
+        .then(response => response.json())
+        .then(data =>{
+
+            data.forEach(cat =>{
+
+                const newLi = document.createElement('li');
+                newLi.textContent = `name of cat: ${cat.name}  || age: ${cat.age}`;
+                catList.append(newLi);
+            })
+        }); 
+    }
+
+
+    // lancer la f et interval pour refresh:
+    loadCatList();
+
+    setInterval(loadCatList, 10000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
