c91dd8e43f037b69ff0fc9c1e7023e9bc3dbab95
kottak msi <kottakcode@gmail.com>
2025-04-30 22:50:27 +0400

    bordel

---
+++ assets/js/navbar.js
@@ -0,0 +1,16 @@
+
+function addNavBar() {
+
+    fetch('/assets/components/navbar.html')
+    .then(response => response.text())
+    .then(html =>{
+
+        document.getElementById('navbar-container').innerHTML = html;
+    })
+    .catch(error => {
+        console.error('Erreur lors du chargement de la navbar:', error);
+      });
+
+}
+
+document.addEventListener('DOMContentLoaded', addNavBar);
